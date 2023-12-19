70eeb3c2f5017c1c7c56b28bf0b1c0a533c3a091
danil <danila_monster@mail.ru>
2023-12-19 17:28:36 +0300

    comless4

---
+++ src/main/java/less4/Task01.java
@@ -0,0 +1,11 @@
+package less4;
+
+public class Task01 {
+    public static void main(String[] args) {
+        //Db.con();
+       //Db.workBilder();
+        //Db.sessionConnector();
+       // Db.gettingDataFromTable();
+        Db.deleteOb();
+    }
+}
